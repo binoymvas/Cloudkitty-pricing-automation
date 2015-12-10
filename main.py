7194a0bd651b412928ece4135ed1721cea2e8810
Muralidharan <muraliselva.10@gmail.com>
2015-12-10 17:12:46 +0530

    Modificaitons - adding conf file , workflow change
    Modified the workflow in such a way that details should be loaded from a conf file where user can be able to provide the details.
    And have done the necessary modifications in workflow.

--- main.py
+++ main.py
@@ -1,69 +1,109 @@
+#!/usr/bin/env python
+#
+# Creating a Pricing in Cloudkkity
+# Automated script for Configuring prices in Cloudkitty
+#
+# Author: Muralidharan.S
+#
 from cloudkittyclient import client
+import ConfigParser
 
-# Initializing or establishing the connection
+# For importing details from config file
+config = ConfigParser.RawConfigParser()
+config.read('cloudkitty_pricing.config')
+
+# Fetch details from config file
+# For connection part
+connection = dict(config.items("connection"))
+
+# Initializing the connection
 kwargs = {
-    "tenant_name":"admin",
-    "auth_url":"http://127.0.0.1:5000/v2.0",
-    "username":"admin",
-    "password":"sop52maw",
+    "tenant_name":connection['tenant_name'],
+    "auth_url":connection['auth_url'],
+    "username":connection['username'],
+    "password":connection['password'],
 }
 
 # creation of service args
-fields_service = {
-        "name": "compute",
+service_args = {
+    "name": "compute",
 }
 
-# client get
-ck = client.get_client("1", **kwargs)
+# Establish the connection                                                                                                                                                                                              
+ck = client.get_client("1", **kwargs)                                                                                                                                                                      
+                                                                                                                                                                                                           
+# service creation                                                                                                                                                                                   
+service_creation = ck.hashmap.services.create(**service_args)                                                                                                                                            
+                                                                                                                                                                                                           
+# service id fetch                                                                                                                                                                                         
+service_id = service_creation.service_id                                                                                                                                                                   
+                                                                                                                                                                                                           
+# flavor field args                                                                                                                                                                                        
+fields_flavor = {                                                                                                                                                                                          
+    "name": "flavor",
+    "service_id": service_id,
+}                                                                                                                                                                                                          
+                                                                                                                                                                                                           
+# flavor field creation                                                                                                                                                                                    
+flavor_field_creation = ck.hashmap.fields.create(**fields_flavor)
 
-# service creation
-service_creation = ck.hashmap.services.create(**fields_service)
+# flavor id fetch
+flavor_field_id = flavor_field_creation.field_id
 
-# service id
-service_id = service_creation.service_id
-
-# flavor field args
-fields_flavor = {
-        "name": "flavor",
-        "service_id": service_id,
+# image_field args
+image_field_args = {
+    "name": "image_id",
+    "service_id": service_id,
 }
 
-# flavor field creation
-flavor_creation = ck.hashmap.fields.create(**fields_flavor)
+# image_field_id field creation
+image_field_id_creation = ck.hashmap.fields.create(**image_field_args)
 
-# flavor id
-flavor_id = flavor_creation.field_id
-
-# image_id field args
-fields_image = {
-        "name": "image_id",
-        "service_id": service_id,
-}
-
-# image_id field creation
-image_id_field = ck.hashmap.fields.create(**fields_image)
-
-# image_id field
-image_id = image_id_field.field_id
+# image_field_id fetch
+image_field_id = image_field_id_creation.field_id
 
 # group field args
-fields_group = {
-        "name": "instance_uptime",
+group_fields_args = {
+    "name": "instance_uptime",
 }
 
 # group field creation
-group_field = ck.hashmap.groups.create(**fields_group)
+group_field_creation = ck.hashmap.groups.create(**group_fields_args)
 
-# group_id
-group_id = group_field.group_id
+# group_id fetch
+group_id = group_field_creation.group_id
 
-# mapping for instance size args
-args_to_rate_creation_instance_size = {
-        'cost': '1.5',
-        'value': 'm1.small',
-        'field_id': flavor_id,
+# Fetch the arguments for Instance size section from conf file
+instance_size_section = dict(config.items("instance_size_section"))
+
+# create the mapping for instance size section with existing values
+for size, cost in instance_size_section.items():
+
+    # args for instance size section
+    args_to_rate_creation_instance_size = {
+        'cost': cost,
+        'value': size,
+        'field_id': flavor_field_id,
         'group_id': group_id,
-}
+    }
 
-# mapping creation for instance size price
-mapping_fields_instance =  ck.hashmap.mappings.create(**args_to_rate_creation_instance_size)
+    # mapping creation for instance size price
+    mapping_fields_instance = ck.hashmap.mappings.create(**args_to_rate_creation_instance_size)
+
+# Fetch the arguments for image pricing section from conf file
+image_section = dict(config.items("image_section"))
+
+# create the mapping for image Pricing section with existing value
+for id, cost in image_section.items():
+
+    # mapping for image
+    args_to_rate_image = {
+        'cost': cost,
+        'value': id,
+        'field_id':image_field_id,
+        'group_id': group_id,
+        'type': 'rate',
+    }
+
+    # mapping creation for image price
+    mapping_fields_image = ck.hashmap.mappings.create(**args_to_rate_image)
